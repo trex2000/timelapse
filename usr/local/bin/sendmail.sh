1ed6ba0dfe41777ec4813e415074fa064d383f7e
trex2000 <trex_daemon@yahoo.com>
2020-07-07 14:07:23 +0300

    added feature to upload to hosting service
    Added feature to upload video to hosting service file.io

--- usr/local/bin/sendmail.sh
+++ usr/local/bin/sendmail.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-MAIL_TO=sample@gmail.com
-MAIL_TO_FAILURE=sample_failure@gmail.com
+MAIL_TO=mailto@email.com
+MAIL_TO_FAILURE=failure@email.com
 SUBJECT="[TIMELAPSE]"
-SNAPSHOT_PATH=/mnt/500gb/Movies/Timelapse
-MESSAGE_BODY="This is the last snapshot from Timelapse"
-
-#do not modify below
+SNAPSHOT_PATH=/path/to/Timelapse
+MESSAGE_BODY="This is the last snapshot from Timelapse. You can download the timelapse video anytime in the next 14 days (but link expires after first download!)  from the link below:"
+#username and password when direct link is provided for BASIC AUTH
+HTTP_USERNAME=username
+HTTP_PASSWORD=password
+#Message to provide when video upload failed and direct link from server is provided
+MESSAGE_BODY_NO_VIDEO_UPLOAD="This is the last snapshot from Timelapse.  You can download the timelapse video from the link below: https://$HTTP_USERNAME:$HTTP_PASSWORD@www.hostname.com/path_to_timelapse/"
+#url of the file upload service
+URL="https://file.io"
+#how long shall the link be available
+DEFAULT_EXPIRE="14d" 
+#do not modify below!!!!!
 
 if grep -qs '/mnt/500gb' /proc/mounts; then
     echo "Drive is mounted."
@@ -27,11 +35,25 @@ if [ ! -f "$SNAPSHOT_PATH/lastsnap.jpg" ]; then
     echo "Timelapse error. Snapshot files doesn't exist." | mail -s $SUBJECT  $MAIL_TO_FAILURE
     exit
 fi
+VIDEO_FILE_PATH=$(find $SNAPSHOT_PATH -name "*avi" -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==1 {print $2}')
+if [ ! -f "$VIDEO_FILE_PATH" ]; then
 
-if [ ! -f "$VIDEO_FILE" ]; then
-    echo "Timelapse error. Video file  doesn't exist." | mail -s $SUBJECT  $MAIL_TO_FAILURE
     exit
 fi
+VIDEO_FILE_NAME=$(basename -- "$VIDEO_FILE_PATH")
+EXPIRE=${2:-$DEFAULT_EXPIRE}
+RESPONSE=$(curl -# -F "file=@${VIDEO_FILE_PATH}" "${URL}/?expires=${EXPIRE}")
+# Response looks like this: {"success":true,"key":"EhzvHTCg","link":"https://file.io/EhzvHTCg","expiry":"14 days"}
+PATTERN="success"
+if [ -z "${RESPONSE##*$PATTERN*}" ] ;then  #upload was successful
+    #trim heading
+    RESPONSE=${RESPONSE#*\"link\":}
+    #trim tail
+    RESPONSE=${RESPONSE%%,\"expiry\"*}
+    echo $MESSAGE_BODY $RESPONSE | mail -s $SUBJECT  $MAIL_TO $MAIL_TO_FAILURE -A "$SNAPSHOT_PATH/lastsnap.jpg"
+else
+    echo "Timelapse error. Video file could not be uploaded. Website returned" $RESPONSE  | mail -s $SUBJECT  $MAIL_TO_FAILURE
+    echo $MESSAGE_BODY_NO_VIDEO_UPLOAD$VIDEO_FILE_NAME | mail -s $SUBJECT  $MAIL_TO $MAIL_TO_FAILURE -A "$SNAPSHOT_PATH/lastsnap.jpg"
+fi
 
-VIDEO_FILE=$(find $SNAPSHOT_PATH -name "*avi" -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==1 {print $2}')
-echo $MESSAGE_BODY | mail -s $SUBJECT  $MAIL_TO $MAIL_TO_FAILURE -A "$SNAPSHOT_PATH/lastsnap.jpg" -A $VIDEO_FILE
+
